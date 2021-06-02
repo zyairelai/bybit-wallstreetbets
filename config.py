3b00a76be0520e68a7de9fe5d4ea9cee0aa3d8f4
Zyaire <zyairelai@gmail.com>
2021-06-02 17:41:28 +0800

    Lower Leverage

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ quantity = [0.001, 0.01 , 0.05, 50]
 leverage, pair = [], []
 for i in range(len(coin)):
     pair.append(coin[i] + "USDT")
-    if   coin[i] == "BTC": leverage.append(40)
-    elif coin[i] == "ETH": leverage.append(30)
-    else: leverage.append(20)
+    if   coin[i] == "BTC": leverage.append(25)
+    elif coin[i] == "ETH": leverage.append(20)
+    else: leverage.append(15)
 
     print("Pair Name        :   " + pair[i])
     print("Trade Quantity   :   " + str(quantity[i]) + " " + coin[i])
