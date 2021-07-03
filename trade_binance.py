ee7b080d3ab5f14836828155e2796274b47383ec
Zyaire <zyairelai@gmail.com>
2021-07-03 09:15:04 +0800

    exit with upward movement

--- trade_binance.py
+++ trade_binance.py
@@ -59,11 +59,13 @@ def GO_SHORT_CONDITION(klines, low, mid, high):
         binance_futures_api.current_close(klines) < EMA.MIDDLE(low, mid, high): return True
 
 def EXIT_LONG_CONDITION(klines, low, mid, high):
-    if  binance_futures_api.current_close(klines) < EMA.HIGHEST(low, mid, high) and \
+    if EMA.DELTA_DOWNWARD(low, mid, high) and \
+        binance_futures_api.current_close(klines) < EMA.HIGHEST(low, mid, high) and \
         binance_futures_api.candle_color(klines) == "RED": return True
 
 def EXIT_SHORT_CONDITION(klines, low, mid, high):
-    if  binance_futures_api.current_close(klines) > EMA.LOWEST(low, mid, high) and \
+    if EMA.DELTA_UPWARD(low, mid, high) and \
+        binance_futures_api.current_close(klines) > EMA.LOWEST(low, mid, high) and \
         binance_futures_api.candle_color(klines) == "GREEN": return True
 
 # ==========================================================================================================================================================================
--- trade_bybit.py
+++ trade_bybit.py
@@ -60,11 +60,13 @@ def GO_SHORT_CONDITION(klines, low, mid, high):
         bybit_api.current_close(klines) < EMA.MIDDLE(low, mid, high): return True
 
 def EXIT_LONG_CONDITION(klines, low, mid, high):
-    if  bybit_api.current_close(klines) < EMA.HIGHEST(low, mid, high) and \
+    if EMA.DELTA_DOWNWARD(low, mid, high) and \
+        bybit_api.current_close(klines) < EMA.HIGHEST(low, mid, high) and \
         bybit_api.candle_color(klines) == "RED": return True
 
 def EXIT_SHORT_CONDITION(klines, low, mid, high):
-    if  bybit_api.current_close(klines) > EMA.LOWEST(low, mid, high) and \
+    if EMA.DELTA_UPWARD(low, mid, high) and \
+        bybit_api.current_close(klines) > EMA.LOWEST(low, mid, high) and \
         bybit_api.candle_color(klines) == "GREEN": return True
 
 # ==========================================================================================================================================================================
