bc5403041444316a3dbb1a005db461440eb5f91b
zyairelai <zyairelai@gmail.com>
2021-10-21 23:59:47 +0800

    Bybit OK

--- run_bybit.py
+++ run_bybit.py
@@ -1,6 +1,5 @@
-import api_binanceusdm
+import api_bybit
 import config, strategy
-import requests, socket, urllib3
 from datetime import datetime
 from termcolor import colored
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -10,34 +9,34 @@ else: print(colored("THIS IS BACKTESTING\n", "red"))
 
 def lets_make_some_money():
     for i in range(len(config.coin)): 
-        dataset = strategy.dataset(i)
-        response = api_binanceusdm.position_information(i)
-        if response[0].get('leverage') != config.leverage[i]: api_binanceusdm.change_leverage(i)
-        if response[1].get('leverage') != config.leverage[i]: api_binanceusdm.change_leverage(i)
-        if not response[0].get('is_isolated'): api_binanceusdm.change_margin_to_ISOLATED(i)
-        if not response[1].get('is_isolated'): api_binanceusdm.change_margin_to_ISOLATED(i)
+        klines_4HOUR = strategy.klines_4HOUR(i)
+        response = api_bybit.position_information(i)
+        if response[0].get('leverage') != config.leverage[i]: api_bybit.change_leverage(i)
+        if response[1].get('leverage') != config.leverage[i]: api_bybit.change_leverage(i)
+        if not response[0].get('is_isolated'): api_bybit.change_margin_to_ISOLATED(i)
+        if not response[1].get('is_isolated'): api_bybit.change_margin_to_ISOLATED(i)
 
-        if api_binanceusdm.LONG_SIDE(response) == "NO_POSITION":
-            if strategy.GO_LONG_CONDITION(dataset):
-                api_binanceusdm.market_open_long(i)
+        if api_bybit.LONG_SIDE(response) == "NO_POSITION":
+            if strategy.GO_LONG_CONDITION(klines_4HOUR):
+                api_bybit.market_open_long(i)
                 print(colored("🚀 GO_LONG 🚀", "green"))
             else: print("LONG_SIDE : 🐺 WAIT 🐺")
 
-        if api_binanceusdm.LONG_SIDE(response) == "LONGING":
-            if strategy.EXIT_LONG_CONDITION(dataset):
-                api_binanceusdm.market_close_long(i, response)
+        if api_bybit.LONG_SIDE(response) == "LONGING":
+            if strategy.EXIT_LONG_CONDITION(klines_4HOUR):
+                api_bybit.market_close_long(i, response)
                 print("💰 CLOSE_LONG 💰")
             else: print(colored("HOLDING_LONG", "green"))
 
-        if api_binanceusdm.SHORT_SIDE(response) == "NO_POSITION":
-            if strategy.GO_SHORT_CONDITION(dataset):
-                api_binanceusdm.market__open_short(i)
+        if api_bybit.SHORT_SIDE(response) == "NO_POSITION":
+            if strategy.GO_SHORT_CONDITION(klines_4HOUR):
+                api_bybit.market__open_short(i)
                 print(colored("💥 GO_SHORT 💥", "red"))
             else: print("SHORT_SIDE : 🐺 WAIT 🐺")
 
-        if api_binanceusdm.SHORT_SIDE(response) == "SHORTING":
-            if strategy.EXIT_SHORT_CONDITION(dataset):
-                api_binanceusdm.market_close_short(i, response)
+        if api_bybit.SHORT_SIDE(response) == "SHORTING":
+            if strategy.EXIT_SHORT_CONDITION(klines_4HOUR):
+                api_bybit.market_close_short(i, response)
                 print("💰 CLOSE_SHORT 💰")
             else: print(colored("HOLDING_SHORT", "red"))
 
@@ -49,13 +48,5 @@ try:
         scheduler.add_job(lets_make_some_money, 'cron', minute='0,10,20,30,40,50')
         scheduler.start()
     else: lets_make_some_money()
-
-except (socket.timeout,
-        urllib3.exceptions.ProtocolError,
-        urllib3.exceptions.ReadTimeoutError,
-        requests.exceptions.ConnectionError,
-        requests.exceptions.ConnectTimeout,
-        requests.exceptions.ReadTimeout,
-        ConnectionResetError, KeyError, OSError) as e: print(e)
 
 except KeyboardInterrupt: print("\n\nAborted.\n")
--- strategy.py
+++ strategy.py
@@ -1,15 +1,14 @@
 import config, ccxt, pandas
 ccxt_client = ccxt.binance()
-current, previous, query = -1, -2, 54
+
+query = 100
 tohlcv_column = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
 
-def dataset(i):
+def klines_4HOUR(i):
     print(config.pair[i])
     klines = pandas.DataFrame(ccxt_client.fetch_ohlcv(config.pair[i], '4h', limit=query), columns=tohlcv_column)
-    klines["color"] = klines.apply(candle_color, axis=1)
-    
-    GO_LONG_CONDITION(klines)
-    print(klines)
+    klines['color'] = klines.apply(candle_color, axis=1)
+    return klines
 
 def candle_color(klines):
     if klines['open'] > klines['close']: return "RED"
@@ -17,30 +16,27 @@ def candle_color(klines):
     else: return "INDECISIVE"
 
 def Moving_Average_of_Bitcoin():
-    bitcoin = pandas.DataFrame(ccxt_client.fetch_ohlcv("BTC/USDT", '4h', limit=query), columns=tohlcv_column)
-    bitcoin['Moving Average'] = bitcoin.iloc[:,1].rolling(window=50).mean()
-    # print(bitcoin)
+    bitcoin = pandas.DataFrame(ccxt_client.fetch_ohlcv("BTC/USDT", '1d', limit=query), columns=tohlcv_column)
+    bitcoin['Moving_Avg'] = bitcoin["close"].rolling(window=50).mean()
+    bitcoin = bitcoin.drop(['volume'], axis=1)
     return bitcoin
 
 def GO_LONG_CONDITION(dataset):
-    if (dataset['color'].iloc[current] == "GREEN" and dataset['color'].iloc[previous] == "GREEN" and dataset['color'].iloc[-3] == "GREEN") and \
-        dataset['close'].iloc[current] > Moving_Average_of_Bitcoin()['Moving Average'].iloc[current]: return True
+    if (dataset['close'].iloc[-1] > dataset['close'].iloc[-2] and dataset['close'].iloc[-1] > dataset['close'].iloc[-3]) and \
+        dataset['close'].iloc[-1] > Moving_Average_of_Bitcoin()['Moving_Avg'].iloc[-1]: return True
     else: return False
 
 def GO_SHORT_CONDITION(dataset):
-    if (dataset['color'].iloc[current] == "RED" and dataset['color'].iloc[previous] == "RED" and dataset['color'].iloc[-3] == "RED") and \
-        dataset['close'].iloc[current] < Moving_Average_of_Bitcoin()['Moving Average'].iloc[current]: return True
+    if (dataset['close'].iloc[-1] < dataset['close'].iloc[-2] and dataset['close'].iloc[-1] < dataset['close'].iloc[-3]) and \
+        dataset['close'].iloc[-1] < Moving_Average_of_Bitcoin()['Moving_Avg'].iloc[-1]: return True
     else: return False
 
 def EXIT_LONG_CONDITION(dataset):
-    if (dataset['color'].iloc[current] == "RED" and dataset['color'].iloc[previous] == "RED" and dataset['color'].iloc[-3] == "RED") or \
-        dataset['close'].iloc[current] < Moving_Average_of_Bitcoin()['Moving Average'].iloc[current]: return True
+    if (dataset['close'].iloc[-1] < dataset['close'].iloc[-2] and dataset['close'].iloc[-1] < dataset['close'].iloc[-3]) and \
+        dataset['close'].iloc[-1] < Moving_Average_of_Bitcoin()['Moving_Avg'].iloc[-1]: return True
     else: return False
 
 def EXIT_SHORT_CONDITION(dataset):
-    if (dataset['color'].iloc[current] == "GREEN" and dataset['color'].iloc[previous] == "GREEN" and dataset['color'].iloc[-3] == "GREEN") or \
-        dataset['close'].iloc[current] > Moving_Average_of_Bitcoin()['Moving Average'].iloc[current]: return True
+    if (dataset['close'].iloc[-1] > dataset['close'].iloc[-2] and dataset['close'].iloc[-1] > dataset['close'].iloc[-3]) and \
+        dataset['close'].iloc[-1] > Moving_Average_of_Bitcoin()['Moving_Avg'].iloc[-1]: return True
     else: return False
-
-dataset(0)
-# Moving_Average_of_Bitcoin()
