f578e262d7e5527ded780804f5b9a686d1955b53
zyairelai <zyairelai@gmail.com>
2021-10-29 22:39:26 +0800

    12 hours and 2 hours

--- backtest.py
+++ backtest.py
@@ -3,6 +3,8 @@ import strategy
 import retrieve_klines
 from datetime import datetime
 
+fees = 0.15
+
 def backtest():
     print("\n_Big_ Timeframe : " + retrieve_klines.big_timeframe)
     print("Entry Timeframe : " + retrieve_klines.entry_timeframe + "\n")
@@ -13,22 +15,20 @@ def backtest():
         swing_trades = strategy.swing_trade(i, klines)
         # print(swing_trades)
 
-        print(config.pair[i])
+        print("\n\n" + config.pair[i])
         print("Start Time Since " + str(datetime.fromtimestamp(swing_trades["timestamp"].iloc[0]/1000)))
         long_result = round(check_for_long(i, swing_trades), 2)
         short_reult = round(check_for_short(i, swing_trades), 2)
         overall_result = round(long_result + short_reult, 2)
         all_pairs = round(all_pairs + overall_result, 2)
 
-        print("PNL for _Long Positions: " + str(long_result) + "%")
-        print("PNL for Short Positions: " + str(short_reult) + "%")
-        print("PNL for _BOTH Positions: " + str(overall_result) + "%")
-        print()
+        print("PNL for _BOTH Positions: " + str(overall_result) + "%\n")
     print("ALL PAIRS PNL : " + str(all_pairs) + "%\n")
 
 def check_for_long(i, swing_trades):
     position = False
-    total_pnl, realized_pnl, entry_price = 0, 0, 0
+    total_pnl, total_trades, total_liq = 0, 0, 0
+    wintrade, losetrade = 0, 0
 
     for index in range(len(swing_trades)):
         if not position:
@@ -39,15 +39,32 @@ def check_for_long(i, swing_trades):
             liquidation = (swing_trades['low'].iloc[index] - entry_price) / entry_price * 100 * config.leverage[i] < -80
             if swing_trades["EXIT_LONG"].iloc[index] or liquidation:
                 position = False
-                if liquidation: realized_pnl = -100
-                else : realized_pnl = ((swing_trades['open'].iloc[index] - entry_price) / entry_price * 100 * config.leverage[i]) - (0.2 * config.leverage[i])
+                if liquidation:
+                    realized_pnl = -100
+                    total_liq = total_liq + 1
+                else : realized_pnl = ((swing_trades['open'].iloc[index] - entry_price) / entry_price * 100 * config.leverage[i]) - (fees * config.leverage[i])
+                
+                if realized_pnl > 0.2 * config.leverage[i]: wintrade = wintrade + 1
+                else: losetrade = losetrade + 1
+                
+                total_trades = total_trades + 1
                 total_pnl = total_pnl + realized_pnl
 
+    print("PNL for _Long Positions: " + str(round(total_pnl, 2)) + "%")
+    print("Total  Executed  Trades: " + str(round(total_trades, 2)))
+    print("Total Liquidated Trades: " + str(round(total_liq)))
+    print("_Win Trades: " + str(wintrade))
+    print("Lose Trades: " + str(losetrade))
+    if (wintrade + losetrade > 1):
+        winrate = round(wintrade / (wintrade + losetrade) * 100)
+        print("Winrate : " + str(winrate) + " %")
+    print()
     return round(total_pnl, 2)
 
 def check_for_short(i, swing_trades):
     position = False
-    total_pnl, realized_pnl, entry_price = 0, 0, 0
+    total_pnl, total_trades, total_liq = 0, 0, 0
+    wintrade, losetrade = 0, 0
 
     for index in range(len(swing_trades)):
         if not position:
@@ -58,10 +75,26 @@ def check_for_short(i, swing_trades):
             liquidation = (swing_trades['high'].iloc[index] - entry_price) / entry_price * 100 * config.leverage[i] < -80
             if swing_trades["EXIT_SHORT"].iloc[index] or liquidation:
                 position = False
-                if liquidation: realized_pnl = -100
-                else : realized_pnl = ((swing_trades['open'].iloc[index] - entry_price) / entry_price * 100 * config.leverage[i]) - (0.2 * config.leverage[i])
+                if liquidation:
+                    total_liq = total_liq + 1
+                    realized_pnl = -100
+                else: realized_pnl = ((swing_trades['open'].iloc[index] - entry_price) / entry_price * 100 * config.leverage[i]) - (fees * config.leverage[i])
+                
+                if realized_pnl > 0.2 * config.leverage[i]: wintrade = wintrade + 1
+                else: losetrade = losetrade + 1
+                
+                total_trades = total_trades + 1
                 total_pnl = total_pnl + realized_pnl
 
+    print("PNL for Short Positions: " + str(round(total_pnl, 2)) + "%")
+    print("Total  Executed  Trades: " + str(round(total_trades, 2)))
+    print("Total Liquidated Trades: " + str(round(total_liq)))
+    print("_Win Trades: " + str(wintrade))
+    print("Lose Trades: " + str(losetrade))
+    if (wintrade + losetrade > 1):
+        winrate = round(wintrade / (wintrade + losetrade) * 100, 2)
+        print("Winrate : " + str(winrate))
+    print()
     return round(total_pnl, 2)
 
 backtest()
--- retrieve_klines.py
+++ retrieve_klines.py
@@ -3,19 +3,21 @@ import ccxt
 import pandas
 
 query = 200
+exchange = ccxt.binance()
 tohlcv_column = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
 
-big_timeframe   = '1d'
-entry_timeframe = '6h'
+big_timeframe   = '12h'
+entry_timeframe = '2h'
 
 def retrieve_klines(i):
-    return pandas.DataFrame(ccxt.binance().fetch_ohlcv(config.pair[i], entry_timeframe, limit=query), columns=tohlcv_column)
+    return pandas.DataFrame(exchange.fetch_ohlcv(config.pair[i], entry_timeframe, limit=query), columns=tohlcv_column)
 
 def Moving_Average(i):
-    klines = pandas.DataFrame(ccxt.binance().fetch_ohlcv(config.pair[i], big_timeframe, limit=query), columns=tohlcv_column)
+    MA_threshold = 50
+    klines = pandas.DataFrame(exchange.fetch_ohlcv(config.pair[i], big_timeframe, limit=query), columns=tohlcv_column)
     moving_average = pandas.DataFrame()
     moving_average['timestamp'] = klines["timestamp"]
-    moving_average['MA'] = klines["close"].rolling(window=50).mean()
+    moving_average['MA'] = klines["close"].rolling(window=MA_threshold).mean()
     moving_average["color"] = klines.apply(candle_color, axis=1)
     return moving_average
 
