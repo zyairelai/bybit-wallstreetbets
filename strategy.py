2c10719e9232f967d1fcfb0a0bd002c20f7d7fdd
zyairelai <zyairelai@gmail.com>
2021-11-15 14:23:18 +0800

    enhanced strategy

--- strategy.py
+++ strategy.py
@@ -2,34 +2,38 @@ import pandas
 import modules.candlestick
 import modules.heikin_ashi
 
-test_module = False
+test_module = True
 
 def swing_trade(pair):
     # Fetch the raw klines data
-    get_raw = modules.candlestick.get_klines(pair, '1h')
+    main_raw = modules.candlestick.get_klines(pair, '6h')
+    support  = modules.candlestick.get_klines(pair, '1h')
 
     # Process Heikin Ashi & Apply Technical Analysis
-    candlestick = modules.candlestick.candlestick(get_raw)[["timestamp", "open", "high", "low", "close", "volume", "volumeAvg", "color"]].copy()
-    heikin_ashi = modules.heikin_ashi.heikin_ashi(get_raw)[["timestamp", "candle"]].copy()
-    candlestick = candlestick.rename(columns={'color': 'candlestick'})
-    heikin_ashi = heikin_ashi.rename(columns={'candle': 'heikin_ashi'})
-    
-    dataset = pandas.merge_asof(candlestick, heikin_ashi, on='timestamp')
+    main_candle = modules.candlestick.candlestick(main_raw)[["timestamp", "open", "high", "low", "close", "volume", "volumeAvg"]].copy()
+    ha_6_hours  = modules.heikin_ashi.heikin_ashi(main_raw)[["timestamp", "candle"]].copy()
+    ha_6_hours  = ha_6_hours.rename(columns={'candle': 'HA_6hour'})
+    ha_1_hours  = modules.heikin_ashi.heikin_ashi(support)[["timestamp", "candle"]].copy()
+    ha_1_hours  = ha_1_hours.rename(columns={'candle': 'HA_1hour'})
+
+    dataset = pandas.merge_asof(main_candle, ha_6_hours, on='timestamp')
+    dataset = pandas.merge_asof(ha_1_hours, dataset, on='timestamp')
+
     dataset["GO_LONG"] = dataset.apply(GO_LONG_CONDITION, axis=1)
     dataset["GO_SHORT"] = dataset.apply(GO_SHORT_CONDITION, axis=1)
     return dataset
 
 def GO_LONG_CONDITION(klines):
     color = "GREEN"
-    if  klines['candlestick'] == color and \
-        klines['heikin_ashi'] == color and \
+    if  klines['HA_6hour'] == color and \
+        klines['HA_1hour'] == color and \
         klines['volume'] > klines["volumeAvg"]: return True
     else: return False
 
 def GO_SHORT_CONDITION(klines):
     color = "RED"
-    if  klines['candlestick'] == color and \
-        klines['heikin_ashi'] == color and \
+    if  klines['HA_6hour'] == color and \
+        klines['HA_1hour'] == color and \
         klines['volume'] > klines["volumeAvg"]: return True
     else: return False
 
