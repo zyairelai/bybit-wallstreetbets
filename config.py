509df3789e656101a67e4904d5c8f548b46fec8f
zyairelai <zyairelai@gmail.com>
2021-12-08 14:51:48 +0800

    best leverage at 15x

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ quantity = [0.001, 0.01, 0.05, 0.1]
 pair, leverage = [], []
 for i in range(len(coin)):
     pair.append(coin[i] + "USDT")
-    if coin[i] == "BTC": leverage.append(50)
-    elif coin[i] == "ETH": leverage.append(40)
-    else: leverage.append(30)
+    if coin[i] == "BTC": leverage.append(15)
+    elif coin[i] == "ETH": leverage.append(15)
+    else: leverage.append(15)
